0f3220bbf2933a6aecc17c71629066a44a8ee8b2
AnthonyAvila12 <87146715+AnthonyAvila12@users.noreply.github.com>
2021-07-26 15:34:28 -0600

    calculadora modulo 7

---
+++ Tarea_Modulo_Anthony_Avila.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_modulo7_Anthony_Avila;
+
+/**
+ *
+ * @author Anthony Avila
+ */
+public class Tarea_Modulo_Anthony_Avila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
